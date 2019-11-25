baecdfb6b57cd6e57f1a8a70e34af1b66ea2d537
Timothy Swanson <timtheswan@gmail.com>
2019-11-24 21:28:49 -0500

    set up model

--- ride-share/hapi-server.js
+++ ride-share/hapi-server.js
@@ -2,20 +2,30 @@
 const Path = require("path");
 
 // Knex
-const knex = require("knex")({
-  client: "pg",
+
+const knex = require('knex')({
+  client: 'pg',
   connection: {
-    host: "localhost",
-    database: "ui-spa"
-  }
+  host: 'faraday.cse.taylor.edu',
+  user: 'tim_swanson',
+  password: 'wijenara',
+  database: 'tim_swanson'
+   }
 });
 
 // Objection
 const objection = require("objection");
+const Model = objection.Model; //Unknown if needed
 objection.Model.knex(knex);
 
 // Models
-const Account = require("./models/Account");
+const driver = require("./models/Driver");
+const vehicle = require("./models/Vehicle");
+const ride = require("./models/Ride");
+const state = require("./models/State");
+const location = require("./models/Location");
+const vehicle_type = require("./models/Vehicle_type");
+const passenger = require("./models/Passenger");
 
 // Hapi
 const Joi = require("@hapi/joi"); // Input validation
@@ -45,91 +55,91 @@ async function init() {
   await server.register(require("@hapi/inert"));
 
   // Configure routes.
-  server.route([
-    {
-      method: "POST",
-      path: "/accounts",
-      config: {
-        description: "Sign up for an account",
-        validate: {
-          payload: Joi.object({
-            firstName: Joi.string().required(),
-            lastName: Joi.string().required(),
-            email: Joi.string()
-              .email()
-              .required(),
-            password: Joi.string().required()
-          })
-        }
-      },
-      handler: async (request, h) => {
-        const existingAccount = await Account.query()
-          .where("email", request.payload.email)
-          .first();
-        if (existingAccount) {
-          return {
-            ok: false,
-            msge: `Account with email '${request.payload.email}' is already in use`
-          };
-        }
+  // server.route([
+  //   {
+  //     method: "POST",
+  //     path: "/accounts",
+  //     config: {
+  //       description: "Sign up for an account",
+  //       validate: {
+  //         payload: Joi.object({
+  //           firstName: Joi.string().required(),
+  //           lastName: Joi.string().required(),
+  //           email: Joi.string()
+  //             .email()
+  //             .required(),
+  //           password: Joi.string().required()
+  //         })
+  //       }
+  //     },
+  //     handler: async (request, h) => {
+  //       const existingAccount = await Account.query()
+  //         .where("email", request.payload.email)
+  //         .first();
+  //       if (existingAccount) {
+  //         return {
+  //           ok: false,
+  //           msge: `Account with email '${request.payload.email}' is already in use`
+  //         };
+  //       }
 
-        const newAccount = await Account.query().insert({
-          first_name: request.payload.firstName,
-          last_name: request.payload.lastName,
-          email: request.payload.email,
-          password: request.payload.password
-        });
+  //       const newAccount = await Account.query().insert({
+  //         first_name: request.payload.firstName,
+  //         last_name: request.payload.lastName,
+  //         email: request.payload.email,
+  //         password: request.payload.password
+  //       });
 
-        if (newAccount) {
-          return {
-            ok: true,
-            msge: `Created account '${request.payload.email}'`
-          };
-        } else {
-          return {
-            ok: false,
-            msge: `Couldn't create account with email '${request.payload.email}'`
-          };
-        }
-      }
-    },
+  //       if (newAccount) {
+  //         return {
+  //           ok: true,
+  //           msge: `Created account '${request.payload.email}'`
+  //         };
+  //       } else {
+  //         return {
+  //           ok: false,
+  //           msge: `Couldn't create account with email '${request.payload.email}'`
+  //         };
+  //       }
+  //     }
+  //   },
 
-    {
-      method: "GET",
-      path: "/accounts",
-      config: {
-        description: "Retrieve all accounts"
-      },
-      handler: (request, h) => {
-        return Account.query();
-      }
-    },
+  //   {
+  //     method: "GET",
+  //     path: "/accounts",
+  //     config: {
+  //       description: "Retrieve all accounts"
+  //     },
+  //     handler: (request, h) => {
+  //       return Account.query();
+  //     }
+  //   },
 
-    {
-      method: "DELETE",
-      path: "/accounts/{id}",
-      config: {
-        description: "Delete an account"
-      },
-      handler: (request, h) => {
-        return Account.query()
-          .deleteById(request.params.id)
-          .then(rowsDeleted => {
-            if (rowsDeleted === 1) {
-              return {
-                ok: true,
-                msge: `Deleted account with ID '${request.params.id}'`
-              };
-            } else {
-              return {
-                ok: false,
-                msge: `Couldn't delete account with ID '${request.params.id}'`
-              };
-            }
-          });
-      }
-    }
-  ]);
+  //   {
+  //     method: "DELETE",
+  //     path: "/accounts/{id}",
+  //     config: {
+  //       description: "Delete an account"
+  //     },
+  //     handler: (request, h) => {
+  //       return Account.query()
+  //         .deleteById(request.params.id)
+  //         .then(rowsDeleted => {
+  //           if (rowsDeleted === 1) {
+  //             return {
+  //               ok: true,
+  //               msge: `Deleted account with ID '${request.params.id}'`
+  //             };
+  //           } else {
+  //             return {
+  //               ok: false,
+  //               msge: `Couldn't delete account with ID '${request.params.id}'`
+  //             };
+  //           }
+  //         });
+  //     }
+  //   }
+  // ]);
 
   // Start the server.
   await server.start();
